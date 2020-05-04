8c0ac67289bbbf2e9415d9e5c1f06796635836c2
suzukalight <mail@mkubara.com>
2020-05-04 22:05:52 +0900

    Add service tf

---
+++ terraform/service/terraform_remote_state.tf
@@ -0,0 +1,16 @@
+data "terraform_remote_state" "aws_iam" {
+  backend = "s3"
+  config = {
+    bucket = "study-aws-ecr-ecs-terraform-suzukalight"
+    key    = "sample/iam/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "study-aws-ecr-ecs-terraform-suzukalight"
+    key    = "sample/vpc/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
