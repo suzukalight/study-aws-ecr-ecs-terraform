ab98bd069e9ec7024a7c12938df3adb7d52e8d61
suzukalight <mail@mkubara.com>
2020-05-05 08:17:21 +0900

    Add ssl cert and https listener

--- terraform/service/aws_acm_cerificate.tf
+++ terraform/service/aws_acm_cerificate.tf
@@ -2,4 +2,13 @@ resource "aws_acm_certificate" "mokmok" {
   domain_name               = data.aws_route53_zone.mokmok.name
   subject_alternative_names = []
   validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "mokmok" {
+  certificate_arn         = aws_acm_certificate.mokmok.arn
+  validation_record_fqdns = [aws_route53_record.mokmok_certificate.fqdn]
 }
--- terraform/service/aws_lb_listener.tf
+++ terraform/service/aws_lb_listener.tf
@@ -2,8 +2,37 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "80"
   protocol          = "HTTP"
+
   default_action {
-    target_group_arn = aws_lb_target_group.http.arn
     type             = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.mokmok.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  }
+}
+
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
