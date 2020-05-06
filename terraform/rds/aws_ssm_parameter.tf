44b65ebff8f61c51d1344e1104c35d0f1dc69282
suzukalight <mail@mkubara.com>
2020-05-06 18:07:11 +0900

    Fix

--- terraform/rds/aws_ssm_parameter.tf
+++ terraform/rds/aws_ssm_parameter.tf
@@ -1,29 +1,36 @@
+resource "aws_ssm_parameter" "db_hostname" {
+  name        = "/sample/DB_HOSTNAME"
+  value       = aws_db_instance.sample.address
+  type        = "String"
+  description = "DB host name"
+}
+
+resource "aws_ssm_parameter" "db_database" {
+  name        = "/sample/DB_DATABASE"
+  value       = "terraform"
+  type        = "String"
+  description = "DB name"
+}
+
 resource "aws_ssm_parameter" "db_username" {
   name        = "/sample/DB_USERNAME"
   value       = "admin"
   type        = "String"
-  description = "DB USERNAME"
+  description = "DB user"
 }
 
 resource "aws_ssm_parameter" "db_raw_password" {
   name        = "/sample/DB_PASSWORD"
   value       = "uninitialized"
   type        = "SecureString"
-  description = "DB PASSWORD"
+  description = "DB password"
 
   lifecycle {
     ignore_changes = [value]
   }
 }
 
-resource "aws_ssm_parameter" "db_hostname" {
-  name        = "/sample/DB_HOSTNAME"
-  value       = aws_db_instance.sample.address
-  type        = "String"
-  description = "DB HOSTNAME"
-}
-
 # Please overwrite the password manually with the following command;
 # ---
-# $ aws ssm put-parameter --name '/sample/DB_PASSWORD' --type SecureString --value 'ModifiedStrongPassword!' --overwrite
+# $ aws ssm put-parameter --name '/sample/DB_PASSWORD' --type SecureString --overwrite --value 'ModifiedStrongPassword!'
 # ---
