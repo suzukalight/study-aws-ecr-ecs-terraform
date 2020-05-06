d948467bd1fbf0099e90c56072360c938ebf1cad
suzukalight <mail@mkubara.com>
2020-05-06 23:38:47 +0900

    Fix

--- src/infrastructure/dataloader/loaders/user.ts
+++ src/infrastructure/dataloader/loaders/user.ts
@@ -2,9 +2,11 @@ import { Op } from 'sequelize';
 
 import { Models } from '../../../domain/models';
 
-export const batchUsers = async (keys: number[], models: Models) => {
+export const batchUsers = async (keys: readonly number[], models: Models) => {
+  const _keys = keys as number[]; // FIXME: workaround
+
   const users = await models.User.findAll({
-    where: { id: { [Op.in]: keys } },
+    where: { id: { [Op.in]: _keys } },
   });
 
   return keys.map(key => users.find(user => user.id === key) || new Error(`No result for ${key}`));
