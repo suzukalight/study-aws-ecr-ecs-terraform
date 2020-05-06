39bceeaba3edab50b40c2ec04ea89bc9839bdddd
suzukalight <mail@mkubara.com>
2020-05-07 00:03:53 +0900

    Fix ssm and secrets

--- terraform/service/aws_ssm_parameter.tf
+++ terraform/service/aws_ssm_parameter.tf
@@ -4,3 +4,17 @@ resource "aws_ssm_parameter" "jwt_secret" {
   type        = "SecureString"
   description = "JWT Secret"
 }
+
+resource "aws_ssm_parameter" "jwt_expires_in" {
+  name        = "/sample/JWT_EXPIRES_IN"
+  value       = "30m"
+  type        = "String"
+  description = "jwt expires in"
+}
+
+resource "aws_ssm_parameter" "server_port" {
+  name        = "/sample/SERVER_PORT"
+  value       = "23456"
+  type        = "String"
+  description = "server port"
+}
