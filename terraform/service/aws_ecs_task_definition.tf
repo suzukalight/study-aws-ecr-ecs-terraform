95b5af12d72ab68015f1c8e3309e21309aec338c
suzukalight <mail@mkubara.com>
2020-05-06 17:33:08 +0900

    Update ecs tf to use iam module

--- terraform/service/aws_ecs_task_definition.tf
+++ terraform/service/aws_ecs_task_definition.tf
@@ -6,6 +6,8 @@ resource "aws_ecs_task_definition" "sample-task" {
   requires_compatibilities = ["FARGATE"]
 
   container_definitions = file("./container_definitions/service.json")
-  task_role_arn         = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
-  execution_role_arn    = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  # task_role_arn         = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  # execution_role_arn    = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  task_role_arn      = module.ecs_task_execution_role.iam_role_arn
+  execution_role_arn = module.ecs_task_execution_role.iam_role_arn
 }
---
+++ terraform/service/aws_iam_role.tf
@@ -0,0 +1,24 @@
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameters",
+      "secretsmanager:GetSecretValue",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "../modules/iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
