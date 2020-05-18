c84643eb6541a68ea1464f1d58ddb06a8ea20c3e
suzukalight <mail@mkubara.com>
2020-05-18 09:27:00 +0900

    Refactor shellscript to push docker image

--- terraform/docker-push.sh
+++ terraform/docker-push.sh
@@ -13,12 +13,18 @@ echo "AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID"
 echo "PROJECT_NAME=$PROJECT_NAME"
 echo "PROFILE=$PROFILE"
 
+echo ""
+echo "Building and pushing new docker images..."
+
 aws ecr get-login-password --region ap-northeast-1 $PROFILE | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com
 
-export ECR_URI_NGINX=$(aws ecr describe-repositories --repository-names ${PROJECT_NAME}-nginx --query 'repositories[0].repositoryUri' --output text)
-docker build -t ${ECR_URI_NGINX} -f docker/nginx/Dockerfile .
-docker push ${ECR_URI_NGINX}
+ECR_URI_NGINX=$(aws ecr describe-repositories --repository-names ${PROJECT_NAME}-nginx --query 'repositories[0].repositoryUri' --output text)
+docker build -t $ECR_URI_NGINX -f docker/nginx/Dockerfile .
+docker push $ECR_URI_NGINX
 
-export ECR_URI_BACKEND=$(aws ecr describe-repositories --repository-names ${PROJECT_NAME}-backend --query 'repositories[0].repositoryUri' --output text)
-docker build -t ${ECR_URI_BACKEND} -f docker/backend/Dockerfile .
-docker push ${ECR_URI_BACKEND}
+ECR_URI_BACKEND=$(aws ecr describe-repositories --repository-names ${PROJECT_NAME}-backend --query 'repositories[0].repositoryUri' --output text)
+docker build -t $ECR_URI_BACKEND -f docker/backend/Dockerfile .
+docker push $ECR_URI_BACKEND
+
+echo ""
+echo "Succeessfully pushed docker images."
